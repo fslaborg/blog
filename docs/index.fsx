346ef16422d84179b3f80ba2701345ae5a03b35f
Kevin Schneider <schneider.kev@outlook.de>
2022-01-26 16:12:07 +0100

    move blog into separate repo

---
+++ docs/index.fsx
@@ -0,0 +1,17 @@
+(***hide***)
+
+(*
+#frontmatter
+---
+title: -
+category: hidden
+authors: -
+index: 0
+---
+*)
+
+(**
+# This is not the page you are looking for.
+
+Whoops! please tell us how you got here. You are most likely looking for this page: https://fslab.org/tutorials.html
+*)
